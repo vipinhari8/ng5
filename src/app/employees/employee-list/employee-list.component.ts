4bd288a96ff8f96d4484e057b321dc6f2c6fcb56
vipin-cutting-edge <vipin.hari@outlook.com>
2018-03-02 15:11:36 +0530

    employee get funcitonality updated

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-employee-list',
@@ -8,10 +10,25 @@ import { EmployeeService } from '../shared/employee.service';
   providers:[EmployeeService]
 })
 export class EmployeeListComponent implements OnInit {
+  employeeList: Employee[];
 
-  constructor(private employeeService:EmployeeService) { }
+  constructor(private employeeService:EmployeeService) {}
 
   ngOnInit() {
+    var emp = this.employeeService.getData();
+    emp.snapshotChanges().subscribe(item=>{
+      this.employeeList = [];
+      item.forEach(element=>{
+        var empData = element.payload.toJSON();
+        empData["$key"] = element.key;
+        this.employeeList.push(empData as Employee);
+      });
+    });
+  }
+  onEdit(emp:Employee){
+    debugger
+    this.employeeService.selectedEmployee = emp;
+    //this.employeeService.selectedEmployee = Object.assign({},emp);
   }
 
 }
